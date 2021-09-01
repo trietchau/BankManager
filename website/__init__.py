dfa5bbf518015ef97f06db7efc47fb5eaa6d5b19
TCadillac <chauminh.triet1702@gmail.com>
2021-09-01 22:34:21 +0700

    Add route to login, signup, and logout

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,11 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "This is my secret key"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix ="/")
+    app.register_blueprint(auth, url_prefix ="/")
+
+
     return app
